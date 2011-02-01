268439c8778a111ea0a63d811be3c9474f0bc305
Ming-Ting Yao Wei <mwei@lxde.org>
2011-02-01 10:25:12 +0800

    sort the bus lines eta by time

--- getstopbus.py
+++ getstopbus.py
@@ -8,9 +8,11 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from django.utils import simplejson
+from operator import itemgetter
 
 class MainHandler(webapp.RequestHandler):
 	def get(self):
+		self.response.headers.add_header("Cache-Control", "no-cache")
 		stopName = self.request.get('stopName').encode('utf-8')
 		req = urllib2.Request('http://www.taipeibus.taipei.gov.tw/Asp/GetTimeByRouteStop4.aspx?GSName=%s' % stopName)
 		response = urllib2.urlopen(req)
@@ -25,8 +27,13 @@ class MainHandler(webapp.RequestHandler):
 			routeName=routeName.decode('utf-8').strip(" _")
 			isReturn=int(isReturn.strip(" _")) == 1
 			estTime=int(estTime.strip(" _"))
+
+			if estTime<0:
+				estTime=86400
+
 			a.append({'routeName': routeName, 'isReturn': isReturn, 'estTime': estTime})
 
+		a=sorted(a,key=itemgetter('estTime'))
 		self.response.out.write(simplejson.dumps(a))
 
 def main():
